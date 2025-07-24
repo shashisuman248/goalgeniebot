6c78b38ce5d076575ee907efcc4cd1b86b8749a1
shashisuman248 <84244969+shashisuman248@users.noreply.github.com>
2025-07-24 17:09:47 +0530

    Create app.py
    Added app.py with basic reply logic

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+import os
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "GoalGenieBot is running ✅"
+
+@app.route("/whatsapp/webhook", methods=["POST"])
+def whatsapp_webhook():
+    incoming_msg = request.values.get('Body', '').lower()
+    print("User said:", incoming_msg)
+
+    resp = MessagingResponse()
+    msg = resp.message()
+
+    # Simple reply
+    msg.body("This is GoalGenieBot powered by Sip Wealth 🤖\nHow can I help you with your mutual fund goals today?")
+
+    return str(resp)
